b8fdf1c5e7c2c624ae11c14803ed488ee9c94796
livmikala <livgun01@iba.dk>
2026-02-23 13:41:49 +0100

    ScoreDisplay component tilføjet

---
+++ Vue-opgave5/components/ScoreDisplay.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>Spiller 1: {{ score1 }}</p>
+        <p>Spiller 2: {{ score2 }}</p>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        score1: Number,
+        score2: Number,
+        message: String
+    }
+}
+</script>
+
+<style>
+p { font-size: 18px; }
+</style>
