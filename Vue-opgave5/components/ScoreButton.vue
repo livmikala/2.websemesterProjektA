3143423055dbb66437ffdfba09dcf5ebea6d18e0
livmikala <livgun01@iba.dk>
2026-02-23 13:56:48 +0100

    ScoreButton component tilføjet

---
+++ Vue-opgave5/components/ScoreButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <button @click="$emit('add-point')" :disabled="disabled">
+        {{ label }}
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        label: String,
+        disabled: Boolean
+    }
+}
+</script>
+
+<style>
+button {
+    margin: 5px;
+    font-size: 16px;
+    padding: 5px 10px;
+}
+</style>
