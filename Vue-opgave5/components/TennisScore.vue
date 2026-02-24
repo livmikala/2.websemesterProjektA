14580d36073b9166cb0f15171115ce3f677a16e1
livmikala <livgun01@iba.dk>
2026-02-24 09:51:00 +0100

    Tilføjer package.json + vue

---
+++ Vue-opgave5/components/TennisScore.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+<ScoreButton
+label="+1 Spiller 1"
+:disabled="gameOver"
+@add-point="addPointPlayer1"
+/>
+
+<ScoreButton
+label="+1 Spiller 2"
+:disabled="gameOver"
+@add-point="addPointPlayer2"
+/>
+
+<ScoreButton
+label="Reset"
+@add-point="resetGame"
+/>
+    </div>
+</template>
+
+<script>
+import ScoreButton from './ScoreButton.vue'
+
+export default {
+    components: { ScoreButton },
+
+data () {
+    return {
+        score1: 0,
+        score2: 0,
+        gameOver: false
+    }
+},
+
+methods: {
+    addPointPlayer1() { this.score1++ },
+    addPointPlayer2() { this.score2++ },
+    resetGame() {
+        this.score1 = 0
+        this.score2 = 0
+        this.gameOver = false
+    }
+}
+}
+</script>
