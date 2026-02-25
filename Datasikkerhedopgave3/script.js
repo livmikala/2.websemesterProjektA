2dd809bd4bc95b7fbffcf191562ef0079a5954e0
livmikala <livgun01@iba.dk>
2026-02-25 10:45:19 +0100

    script.js + server.js tilføjet

---
+++ Datasikkerhedopgave3/script.js
@@ -0,0 +1,34 @@
+const loginForm = document.getElementById('loginForm');
+
+loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+//Sender login request
+
+const loginRes = await fetch('http://localhost:3000/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({ username, password })
+});
+
+if (loginRes.status !== 200) {
+    alert(loginData.message);
+    return;
+}
+
+//Email simulering
+const otp = prompt(`Indtast den OTP du fik(simuleret email): ${loginData.otp}`);
+
+//Send OTP til server
+const verifyRes = await fetch('http://localhost:3000/verify-otp', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, otp })
+});
+
+const verifyData = await verifyRes.json();
+alert(verifyData.message);
+});
--- Datasikkerhedopgave3/server.js
+++ Datasikkerhedopgave3/server.js
@@ -8,4 +8,31 @@ app.use(bodyParser.json());
 let users = JSON.parse(fs.readFileSync('users.json'));
 let tempOTPs = {};
 
-app.post('/login',)
+//Login
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username && u.password === password);
+    if (!user) return res.status(400).json({ message: 'Forkert brugernavn eller password' });
+
+    //Laver engangskode
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    tempOTPs[username] = { otp, used: false };
+
+    //e-mail-simulering
+    res.json({ message: 'OTP genereret, se konsol eller http-respons', otp });
+});
+
+//Verify OTP
+app.post('/verify-otp', (req, res) => {
+    const { username, otp } = req.body;
+    const record = tempOTPs[username];
+
+    if (!record) return res.status(400).json({ message: 'Ingen OTP funder for bruger' });
+    if (record.used) return res.status(400).json({ message: 'Denne OTP er allerede brugt' });
+    if (record.otp != otp) return res.status(400).json({ message: 'Forkert OTP' });
+
+    record.used = true;
+    res.json({ message: 'Login lykkedes'});
+});
+
+app.listen(3000, () => console.log('Server kører på http://localhost:3000'));
